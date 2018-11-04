61a5eaa0f9330f99bc4518cdabb379036f6f63e5
cristinagomezortega <44444635+cristinagomezortega@users.noreply.github.com>
2018-11-04 16:51:22 +0100

    Update edat_3.py

--- phyton.py/edat_3.py
+++ phyton.py/edat_3.py
@@ -1,5 +1,5 @@
-edad= int(input("di tu edad:"))
+edad= int(input("Escribe tu edad:"))
 if (edad <= 23) or (edad >= 18) and (edad ==17) :
-    print("puedes entrar en tu sesion de jovenes")
+    print("Puedes entrar en la sesion de jovenes")
 else:
-    print("lo siento no puedes entrar en la sesion de jovenes")
+    print("Perdone no puede pasar en la de jovenes")
