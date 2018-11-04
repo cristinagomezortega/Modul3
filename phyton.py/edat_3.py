e3ed7d8b8c0a48ea0bccdc813dc9f13a1aadaf85
cristinagomezortega <44444635+cristinagomezortega@users.noreply.github.com>
2018-11-04 16:59:33 +0100

    Delete edat_3.py

--- phyton.py/edat_3.py
+++
@@ -1,5 +0,0 @@
-edad= int(input("Escribe tu edad:"))
-if (edad <= 23) or (edad >= 18) and (edad ==17) :
-    print("Puedes entrar en la sesion de jovenes")
-else:
-    print("Perdone no puede pasar en la de jovenes")
