bd880ba1115a4a84f22ca45bb136c4832ea44b84
cristinagomezortega <44444635+cristinagomezortega@users.noreply.github.com>
2018-11-07 23:13:46 +0100

    Create Años.py

---
+++ Python/Años.py
@@ -0,0 +1,21 @@
+anyo_actual=int(input("Introduzca el año actual:")
+anyo_cualquiera=int(input("Indique el año que ud desee:")
+
+if(anyo_actual=2018)and(anyo_cualquiera=2018):
+    print("Son el mismo anyo")
+else:
+    print("Error")
+                    
+                    
+if(anyo_actual=2020)and(anyo_cualquiera=1999):
+    print("Desde el año 2020 han pasado 21 anyos")
+else:
+    print("Error")
+                    
+                    
+                    
+ if(anyo_actual=2018)and(anyo_cualquiera=2025):
+    print("Para llegar al 2025 faltan 7 anyos")
+else:
+    print("Error")
+                    
